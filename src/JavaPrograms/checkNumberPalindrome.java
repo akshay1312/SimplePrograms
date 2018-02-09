3385be8dd1d2b11fbb33ae1e54a5dc8dfbaf0435
Akshay <apitale@operative.com>
2018-02-09 11:18:09 -0500

    program to find second largest number in array

---
+++ src/JavaPrograms/checkNumberPalindrome.java
@@ -0,0 +1,27 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+/**
+ * Created by apitale on 2018-02-02.
+ */
+public class checkNumberPalindrome {
+
+  public static void main(String[] args) {
+
+    System.out.println("Please Enter a number : ");
+    int givennumber = new Scanner(System.in).nextInt();
+    int number = givennumber;
+    int reverse = 0;
+    while (number != 0) {
+      int remainder = number % 10;
+      reverse = reverse * 10 + remainder;
+      number = number / 10;
+    }
+    if (givennumber == reverse)
+      System.out.println("Result:Palindrome");
+    else
+      System.out.println("Result:Not Palindrome");
+  }
+
+}
