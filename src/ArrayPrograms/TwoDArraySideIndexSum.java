e58550e62a77fc8d135148758b5f6053458baeca
Akshay <apitale@opencrowd.com>
2019-03-24 12:02:56 -0400

    2D array program

---
+++ src/ArrayPrograms/TwoDArraySideIndexSum.java
@@ -0,0 +1,66 @@
+package ArrayPrograms;
+
+import java.util.Arrays;
+
+/**
+ * Sum of array index of right,left,up,down and replace array index with result
+ *
+ * Input Array ::  [1, 2, 3]
+ *                 [4, 5, 6]
+ *                 [7, 8, 9]
+ *
+ * Output Array :: [6, 9, 8]
+ *                 [13, 20, 17]
+ *                 [12, 21, 14]
+ *
+ * @author akshay Date 2019-03-23 20:42
+ */
+public class TwoDArraySideIndexSum {
+
+  public static void main(String args[]) {
+    int row = 3;
+    int col = 3;
+    int[][] input = new int[row][col];
+    int[][] output = new int[row][col];
+    int count = 1;
+
+    //preparing input array
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        input[i][j] = count++;
+      }
+    }
+
+    System.out.println("Input Array :: " + Arrays.deepToString(input));
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        int result = getResult(row, col, input, i, j);
+        output[i][j] = result;
+      }
+    }
+    System.out.println("Output Array :: " + Arrays.deepToString(output));
+
+  }
+
+  private static int getResult(int row, int col, int[][] input, int i, int j) {
+    int result = 0;
+    if (i - 1 >= 0) {
+      result = result + input[i - 1][j];
+    }
+
+    if (i + 1 < col) {
+      result = result + input[i + 1][j];
+    }
+
+    if (j - 1 >= 0) {
+      result = result + input[i][j - 1];
+    }
+
+    if (j + 1 < row) {
+      result = result + input[i][j + 1];
+    }
+    return result;
+  }
+
+
+}
